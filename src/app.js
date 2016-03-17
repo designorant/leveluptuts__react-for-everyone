71f82686239e385c168b3c347e0ab525dc73a022
Michał Ordon <michal@tehidea.com>
2016-03-17 00:46:08 +0000

    #9

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Contacts List</h1>
+            </div>
+        )
+    }
+}
+
+React.render(<App />, document.getElementById('app'));
