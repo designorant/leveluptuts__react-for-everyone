38c884024aaa2d30fb3e1f9302058dfa6cadbca6
Michał Ordon <michal@tehidea.com>
2016-03-17 01:20:13 +0000

    #12

--- src/ContactsList.js
+++ src/ContactsList.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Contact from './Contact';
 
 class ContactsList extends React.Component {
     render() {
         return (
             <ul>
-                <li>{this.props.contacts.name} {this.props.contacts.phone}</li>
+                {this.props.contacts.map((contact) => {
+                    return <Contact contact={contact} key={contact.id} />
+                })}
             </ul>
         )
     }
--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ContactsList from './ContactsList.js';
 
-let contacts = {
-    name: 'Scott',
-    phone: '555 333 5555'
-}
+let contacts = [
+    {
+        id: 1,
+        name: 'Scott',
+        phone: '555 555 5555'
+    },
+    {
+        id: 2,
+        name: 'Courtney',
+        phone: '111 111 5555'
+    },
+    {
+        id: 3,
+        name: 'Tim',
+        phone: '333 333 5555'
+    },
+    {
+        id: 4,
+        name: 'Jeff',
+        phone: '999 333 5555'
+    }
+]
 
 class App extends React.Component {
     render() {
-        console.log(this.props.contacts);
         return (
             <div>
                 <h1>Contacts List</h1>
