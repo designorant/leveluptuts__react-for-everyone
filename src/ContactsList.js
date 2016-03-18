1704ecf272d753a81ae79d07ddeb29345315ddb8
Michał Ordon <michal@tehidea.com>
2016-03-18 19:15:38 +0000

    #13

--- src/ContactsList.js
+++ src/ContactsList.js
@@ -2,13 +2,27 @@ import React from 'react';
 import Contact from './Contact';
 
 class ContactsList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            search: 'Level Up'
+        }
+    }
+
+    updateSearch(event) {
+        this.setState({search: event.target.value.substr(0, 20)});
+    }
+
     render() {
         return (
-            <ul>
-                {this.props.contacts.map((contact) => {
-                    return <Contact contact={contact} key={contact.id} />
-                })}
-            </ul>
+            <div>
+                <ul>
+                    {this.props.contacts.map((contact) => {
+                        return <Contact contact={contact} key={contact.id} />
+                    })}
+                </ul>
+                <input type="text" value={this.state.search} onChange={this.updateSearch.bind(this)} />
+            </div>
         )
     }
 }
