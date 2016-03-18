03c7d2b6fdf6585dd3dc2da713c6585476a6d47d
Michał Ordon <michal@tehidea.com>
2016-03-18 19:31:23 +0000

    #14

--- src/ContactsList.js
+++ src/ContactsList.js
@@ -5,7 +5,7 @@ class ContactsList extends React.Component {
     constructor() {
         super();
         this.state = {
-            search: 'Level Up'
+            search: ''
         }
     }
 
@@ -14,14 +14,19 @@ class ContactsList extends React.Component {
     }
 
     render() {
+        let filteredContacts = this.props.contacts.filter(
+            (contact) => {
+                return contact.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+            }
+        );
         return (
             <div>
+                <input type="text" value={this.state.search} onChange={this.updateSearch.bind(this)} />
                 <ul>
-                    {this.props.contacts.map((contact) => {
+                    {filteredContacts.map((contact) => {
                         return <Contact contact={contact} key={contact.id} />
                     })}
                 </ul>
-                <input type="text" value={this.state.search} onChange={this.updateSearch.bind(this)} />
             </div>
         )
     }
